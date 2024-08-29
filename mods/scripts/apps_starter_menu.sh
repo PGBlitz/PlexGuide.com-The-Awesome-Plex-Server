b73b9810131d722781838079988b9a5e97d0e5d8
Admin9705 <9705@duck.com>
2024-08-29 11:23:43 -0400

    update

--- mods/scripts/apps_starter_menu.sh
+++ mods/scripts/apps_starter_menu.sh
@@ -12,6 +12,24 @@ count_docker_apps() {
     docker ps --format '{{.Names}}' | grep -v 'cf_tunnel' | wc -l
 }
 
+# Load the App Store version from the config file
+load_app_store_version() {
+    if [ -f /pg/config/appstore_version.cfg ]; then
+        source /pg/config/appstore_version.cfg
+    else
+        appstore_version="Alpha"
+    fi
+}
+
+# Function to display the App Store version with appropriate color
+display_app_store_version() {
+    if [ "$appstore_version" == "Alpha" ]; then
+        echo -e "A) App Store Version: [${RED}$appstore_version${NC}]"
+    else
+        echo -e "A) App Store Version: [${GREEN}$appstore_version${NC}]"
+    fi
+}
+
 # Main menu function
 main_menu() {
   while true; do
@@ -20,11 +38,15 @@ main_menu() {
     # Get the number of running Docker apps, excluding cf_tunnel
     APP_COUNT=$(count_docker_apps)
 
+    # Load the App Store version
+    load_app_store_version
+
     echo -e "${BLUE}PG: Docker Apps${NC}"
     echo ""  # Blank line for separation
     # Display the main menu options
     echo -e "V) Apps [${ORANGE}View${NC}] [ $APP_COUNT ]"
     echo -e "D) Apps [${GREEN}Deploy${NC}]"
+    display_app_store_version  # Display App Store Version with appropriate color
     echo "Z) Exit"
     echo ""  # Space between options and input prompt
 
@@ -37,6 +59,9 @@ main_menu() {
         ;;
       D|d)
         bash /pg/scripts/deployment.sh
+        ;;
+      A|a)
+        bash /pg/scripts/apps_version.sh
         ;;
       Z|z)
         exit 0
