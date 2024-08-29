2caa6ee4f16a62b02402a648be66d472cd230008
Admin9705 <9705@duck.com>
2024-08-29 13:08:28 -0400

    color fixes

--- mods/scripts/apps_version.sh
+++ mods/scripts/apps_version.sh
@@ -37,7 +37,7 @@ get_current_version() {
 display_releases() {
     local current_version="$1"
     releases="$2"
-    echo -e "${BLUE}Apps Version Selector - [${NC}${GREEN}${current_version}${NC}]${BLUE}${NC}"
+    echo -e "Apps Version Selector - [${GREEN}${current_version}${NC}]"
     echo "NOTE: Visit https://github.com/plexguide/Apps/releases for Information"
     echo ""
     echo -n -e "${RED}Alpha${NC} "
