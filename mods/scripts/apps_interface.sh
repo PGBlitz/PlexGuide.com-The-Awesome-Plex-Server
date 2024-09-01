46c69119c77bad229496a6520b9b0a0ac8fa3c3f
Admin9705 <9705@duck.com>
2024-09-01 14:23:40 -0400

    update

--- mods/scripts/apps_interface.sh
+++ mods/scripts/apps_interface.sh
@@ -32,9 +32,9 @@ execute_dynamic_menu() {
 
     # Source the app script to load the menu functions
     if [[ "$config_type" == "personal" ]]; then
-        source "/pg/p_apps/${app_name}/${app_name}.app"
+        source "/pg/p_apps/${app_name}.app"
     else
-        source "/pg/apps/${app_name}/${app_name}.app"
+        source "/pg/apps/${app_name}.app"
     fi
 
     # Get the selected option name (e.g., "Plex Token")
