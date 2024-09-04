31cf79fdf50711670ef2015f7dd8114d3d72cbe1
Admin9705 <9705@duck.com>
2024-09-04 17:49:07 -0400

    remove script

--- mods/scripts/apps_interface.sh
+++ mods/scripts/apps_interface.sh
@@ -37,6 +37,40 @@ check_deployment_status() {
     fi
 }
 
+# Function: stop_and_remove_app
+stop_and_remove_app() {
+    while true; do
+        clear
+        echo "This action will stop and remove the Docker container for $app_name."
+        echo "Your appdata will not be lost."
+        echo ""
+        
+        # Generate a random 4-digit code
+        pin_code=$(printf "%04d" $((RANDOM % 10000)))
+        
+        # Prompt the user to confirm with the generated pin code or to cancel with 'Z'
+        echo -e "To proceed, type [${RED}${pin_code}${NC}] to proceed or [${GREEN}Z${NC}] to cancel: "
+        
+        read -p "" user_input
+        
+        if [[ "$user_input" == "$pin_code" ]]; then
+            # If the user enters the correct pin code, proceed to stop and remove the container
+            echo ""
+            echo "Stopping and removing the existing container for $app_name ..."
+            docker stop "$app_name" && docker rm "$app_name"
+            break
+        elif [[ "${user_input,,}" == "z" ]]; then
+            # If the user types 'Z' or 'z', cancel the operation
+            echo "Operation cancelled."
+            break
+        else
+            # If the input is invalid, clear the screen and repeat the prompt
+            clear
+            echo -e "${RED}Invalid input. Please enter the correct pin code or [Z] to cancel.${NC}"
+        fi
+    done
+}
+
 # Function: execute_dynamic_menu
 execute_dynamic_menu() {
     local selected_option=$1
@@ -121,7 +155,7 @@ apps_interface() {
                 bash /pg/scripts/apps_deploy.sh "$app_name" "$config_type"
                 ;;
             k)
-                bash /pg/scripts/apps_kill_remove.sh "$app_name"
+                stop_and_remove_app
                 ;;
             c)
                 bash /pg/scripts/apps_config_menu.sh "$app_name" "$config_type"
