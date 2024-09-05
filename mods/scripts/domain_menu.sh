09e617e50de5ecf94c70add2c18dc2af7444bd2b
Admin9705 <9705@duck.com>
2024-09-04 23:09:11 -0400

    update

--- mods/scripts/domain_menu.sh
+++ mods/scripts/domain_menu.sh
@@ -3,18 +3,21 @@
 # ANSI color codes
 CYAN='\033[0;36m'
 GOLD='\033[0;33m'
-ORANGE='\033[0;91m'
-RED='\033[0;31m'
-BOLD='\033[1m'
+RED="\033[0;31m"
+ORANGE="\033[0;33m"
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+PURPLE="\033[0;35m"
 NC='\033[0m' # No Color
 
 # Function to display the menu
 display_menu() {
     echo -e "${CYAN}PG Domain Configuration Interface${NC}"
     echo
-    echo -e "${BOLD}${GOLD}[C]${NC} CloudFlare Tunnel"
-    echo -e "${BOLD}${ORANGE}[T]${NC} Traefik Reverse Proxy"
-    echo -e "${BOLD}${RED}[Z]${NC} Exit"
+    echo -e "[${BOLD}${GOLD}C${NC}] CloudFlare Tunnel"
+    echo -e "[${BOLD}${BLUE}T${NC}] Traefik Reverse Proxy"
+    echo -e "[${BOLD}${RED}Z${NC}] Exit"
     echo
 }
 
