f0a38d7da6ad02352f94413d054e7ccad368c9c3
Admin9705 <9705@duck.com>
2024-09-13 17:09:56 -0400

    update

--- mods/scripts/domain_menu.sh
+++ mods/scripts/domain_menu.sh
@@ -10,14 +10,34 @@ GREEN="\033[0;32m"
 BLUE="\033[0;34m"
 PURPLE="\033[0;35m"
 NC='\033[0m' # No Color
+BOLD="\033[1m"
+
+# Get the port status from /pg/config/default_ports.cfg
+get_port_status() {
+    config_file="/pg/config/default_ports.cfg"
+    if [[ -f "$config_file" ]]; then
+        source "$config_file"
+        if [[ "$ports" == "open" ]]; then
+            port_status="Open"
+        elif [[ "$ports" == "closed" ]]; then
+            port_status="Closed"
+        else
+            port_status="Unknown"
+        fi
+    else
+        port_status="Unknown"
+    fi
+}
 
 # Function to display the menu
 display_menu() {
     clear
+    get_port_status  # Fetch the port status
     echo -e "${CYAN}PG Domain Configuration Interface${NC}"
     echo
     echo -e "[${YELLOW}${BOLD}A${NC}] CloudFlare Tunnel"
     echo -e "[${CYAN}${BOLD}B${NC}] CloudFlare Traefik"
+    echo -e "[${GREEN}${BOLD}P${NC}] Default Port Protection - (Default Ports: ${port_status})"
     echo -e "[${RED}${BOLD}Z${NC}] Exit"
     echo
 }
@@ -33,6 +53,9 @@ while true; do
             ;;
         [Bb])
             bash /pg/scripts/traefik_menu.sh
+            ;;
+        [Pp])
+            bash /pg/scripts/default_ports.sh
             ;;
         [Zz])
             echo "Exiting..."
