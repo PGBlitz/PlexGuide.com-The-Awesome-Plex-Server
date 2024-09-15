fee89170d75f2a042d284ce2242efe2474eb3115
Admin9705 <9705@duck.com>
2024-09-15 18:27:07 -0400

    update

--- mods/scripts/apps/deploy.sh
+++ mods/scripts/apps/deploy.sh
@@ -98,7 +98,7 @@ while true; do
     echo "" && echo -en "To proceed, enter this PIN [${HOTPINK}${BOLD}${deploy_proceed_code}${NC}]\n"
     echo -en "To cancel, enter this PIN [${GREEN}${BOLD}${deploy_exit_code}${NC}]"
     
-    echo && read -p "Enter PIN > " deploy_choice
+    echo "" && read -p "Enter PIN > " deploy_choice
 
     if [[ "$deploy_choice" == "$deploy_proceed_code" ]]; then
         echo ""
