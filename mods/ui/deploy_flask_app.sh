a47a292fd1d789257f8c13c81566e86f5c96045b
Admin9705 <9705@duck.com>
2024-09-15 16:48:58 -0400

    update

--- mods/ui/deploy_flask_app.sh
+++ mods/ui/deploy_flask_app.sh
@@ -12,9 +12,12 @@ docker rm $APP_NAME > /dev/null 2>&1
 echo "Building the Docker image..."
 docker build -t $APP_NAME .
 
-# Run the Docker container
+# Run the Docker container with a volume mount for /pg/ymals
 echo "Running the Docker container..."
-docker run -d -p 5000:5000 --name $APP_NAME $APP_NAME
+docker run -d -p 5000:5000 \
+  --name $APP_NAME \
+  -v /pg/ymals:/pg/ymals:ro \  # Mount /pg/ymals from host to container (read-only)
+  $APP_NAME
 
 # Confirm the container is running
 if [ "$(docker ps -q -f name=$APP_NAME)" ]; then
